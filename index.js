ce5a2487341f6ae55d7db7902fb97ab3d7465130
Vinícius Carneiro <vinixiussouza@gmail.com>
2023-03-25 12:43:02 -0300

    feat: add middlewares and routes

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Bem vindo ao meu site!');
+// Set 'ejs' as template engine
+app.set("view engine", "ejs");
+// Set static file directory
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/ask", (req, res) => res.render("ask"));
+
+app.post("/save-ask", (req, res) => {
+  const question = req.body.question;
+  res.send("Pergunta cadastrada com sucesso! " + question);
 });
 
 app.listen(port, () => {
-    console.log('Server running on port', port);
+  console.log("Server running on port", port);
 });
